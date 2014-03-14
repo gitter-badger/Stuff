bacf052b060131313eee9f15993065173e7b4e54
U-Bengt\Klong <Klong@Bengt.(none)>
2014-03-15 00:49:39 +0100

    Adding a readme and a image moving script

---
+++ Python/movepics.py
@@ -0,0 +1,18 @@
+import os
+
+# This script moves pictures from the desktop to a folder
+dir = os.getcwd()+"/";
+todir = dir+"Bilder/"
+try:
+	os.mkdir(todir)
+except OSError:
+	print "Directory exists"
+print dir, todir
+for file in os.listdir(dir):
+	filel=file.lower()
+	if filel.endswith(".jpg") or filel.endswith(".png") or filel.endswith(".gif"):
+		try:
+			print dir+file
+			os.rename(dir+file,todir+file)
+		except OSError:
+			print "Error: "+file
---
+++ readme.md
@@ -0,0 +1,3 @@
+Just some random scripts
+Licence is WTFPL unless speficied otherwise
+Use them for what you want
