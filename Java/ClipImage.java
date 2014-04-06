baac97dd477c91fdeddc5d7b308b53b913fb582b
Klong <klong@klong.nu>
2014-04-07 01:05:37 +0200

    ClipImage now saves with hexadecimal names instead

--- Java/ClipImage.java
+++ Java/ClipImage.java
@@ -10,7 +10,7 @@ public class ClipImage {
     public static void main(String[] args) throws Exception {
         Transferable t = Toolkit.getDefaultToolkit().getSystemClipboard().getContents(null);
         BufferedImage image = (BufferedImage) t.getTransferData(DataFlavor.imageFlavor);
-        String name = (System.currentTimeMillis() / 1000) + ".png";
+        String name = Integer.toHexString((int) (System.currentTimeMillis() / 1000L)) + ".png";
         ImageIO.write(image, "png", new File(name));
         System.out.println(name);
     }
